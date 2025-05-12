e20ada9223dc0e9abf0dfeef455d24e1b580a155
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-05-12 15:17:22 +0700

    add type to 'xlsx-populate/browser/xlsx-populate-no-encryption'

---
+++ src/types/xlsx-populate-no-encryption.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module 'xlsx-populate/browser/xlsx-populate-no-encryption' {
+  const XlsxPopulate: any;
+  export = XlsxPopulate;
+}
